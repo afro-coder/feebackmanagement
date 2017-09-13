93b67c611ee2cac254bb96b41e65bb8596603e65
leon9923 <leon9923@gmail.com>
2017-09-13 23:16:30 +0530

    partial template

--- FMSapp/Modules/home/__init__.py
+++ FMSapp/Modules/home/__init__.py
@@ -6,4 +6,4 @@ viewhome = Blueprint('viewhome', __name__)
 @viewhome.route('/index', methods=['GET'])
 @viewhome.route('/',methods=['GET'])
 def home():
-    return render_template('Landing/welcome.html')
+    return render_template('Landing/index.html')
